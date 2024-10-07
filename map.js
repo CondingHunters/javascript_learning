7c7a267f67c989121e6e4e5756a44bde4162f6dd
Abhishek <programwithabhi@gmail.com>
2024-10-07 12:48:52 +0530

    how to use alternate option for map function in js with native for loop

--- map.js
+++ map.js
@@ -4,9 +4,14 @@ let array = [
   { name: "Bhushan", age: 29 },
 ];
 
-let anotherArray = array.map((item) => {
+/*let anotherArray = array.map((item) => {
   //console.log("item is: ", item);
   return item.name;
-});
+});*/
+let anotherArray = [];
+
+for (let index = 0; index < array.length; index++) {
+  anotherArray.push(array[index].name)  
+}
 
 console.log("anotherArray is: ", anotherArray);
