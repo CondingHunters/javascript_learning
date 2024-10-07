543f319cb6dfe9b8e9b9aa1825616fe5fbfb9ded
Abhishek <programwithabhi@gmail.com>
2024-10-07 13:06:22 +0530

    why to use _ in js

---
+++ underscore.js
@@ -0,0 +1,9 @@
+let array = [
+  { name: "Abhishek", age: 22 },
+  { name: "Sourabh", age: 25 },
+  { name: "Bhushan", age: 29 },
+];
+
+array.map((_, index) => {
+  console.log("index is: ", index)
+})
