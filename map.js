7155eb024108d908d17d2e403d275c5347d16b80
Abhishek <programwithabhi@gmail.com>
2024-10-07 12:52:38 +0530

    map callback function defined outside of the map

--- map.js
+++ map.js
@@ -4,14 +4,16 @@ let array = [
   { name: "Bhushan", age: 29 },
 ];
 
-/*let anotherArray = array.map((item) => {
-  //console.log("item is: ", item);
-  return item.name;
-});*/
-let anotherArray = [];
+let callBackFn = (item) => {
+  return item.name
+}
+
+let anotherArray = array.map(callBackFn);
+
+/*let anotherArray = [];
 
 for (let index = 0; index < array.length; index++) {
   anotherArray.push(array[index].name)  
-}
+}*/
 
 console.log("anotherArray is: ", anotherArray);
